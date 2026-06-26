ecb26e04d0e8921b00b11c518687f03191490e81
soc1ly <soc1ly@bk.ru>
2026-06-26 12:00:17 +0700

    Update PlayerJoinListener.java

--- java/ceo/soc1ly/sjointitle/PlayerJoinListener.java
+++ java/ceo/soc1ly/sjointitle/PlayerJoinListener.java
@@ -1,5 +1,6 @@
 package ceo.soc1ly.sjointitle;
 
+import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -18,5 +19,18 @@ public class PlayerJoinListener implements Listener {
         String title = plugin.getConfig().getString("title");
         String subtitle = plugin.getConfig().getString("subtitle");
         event.getPlayer().sendTitle(ColorUtils.colorize(title), ColorUtils.colorize(subtitle), 10, 40, 10);
+
+        String soundName = plugin.getConfig().getString("sound");
+        float volume = (float) plugin.getConfig().getDouble("volume", 1.0);
+        float pitch = (float) plugin.getConfig().getDouble("pitch", 1.0);
+
+        if (soundName != null && !soundName.isEmpty()) {
+            try {
+                Sound sound = Sound.valueOf(soundName);
+                event.getPlayer().playSound(event.getPlayer().getLocation(), sound, volume, pitch);
+            } catch (IllegalArgumentException e) {
+                plugin.getLogger().warning("invalid sound: " + soundName);
+            }
+        }
     }
 }
